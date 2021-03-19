9d798b7a0e8d122f0f84724a3c44860eddd1f5ec
MacAlistair1 <sunbi.mac@gmail.com>
2021-03-19 00:52:48 -0500

    fixings

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
 <h1>Hello</h1>
+
+<button type="button" onclick="triggerAppOpen()">Open App</button>
+
+
+<script>
+    var fallbackToStore = function() {
+        window.location.replace('market://details?id=com.miscommunity.connectfood');
+    };
+    var openApp = function() {
+        window.location.replace('clickeatnpl://clickeat');
+        // setTimeout(fallbackToStore, 250);
+    };
+
+    function triggerAppOpen() {
+        openApp();
+        setTimeout(fallbackToStore, 250);
+    }
+</script>
